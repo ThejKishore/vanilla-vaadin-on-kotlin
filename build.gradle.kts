885fdbb873605c3e5916d97f9da402039f1abc9e
thejkaruneegar <kishores1984@gmail.com>
2025-12-23 00:20:13 -0600

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.2.21"
+    application
+    alias(libs.plugins.vaadin)
+}
+
+defaultTasks("clean", "build")
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
+
+dependencies {
+    // Karibu-DSL dependency
+    implementation(libs.karibu.dsl)
+
+    // Vaadin
+    implementation(libs.vaadin.core)
+    if (!vaadin.effective.productionMode.get()) {
+        implementation(libs.vaadin.dev)
+    }
+    implementation(libs.vaadin.boot)
+    // https://mvnrepository.com/artifact/eu.vaadinonkotlin/vok-rest-client
+    implementation(libs.vaadin.rest.client)
+
+    // logging
+    // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
+    implementation(libs.slf4j.simple)
+
+    // test support
+    testImplementation(libs.karibu.testing)
+    testImplementation(libs.junit)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions.jvmTarget = JvmTarget.JVM_21
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+application {
+    mainClass = "com.example.karibudsl.MainKt"
+}
---
+++ src/main/kotlin/MainView.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.example.karibudsl
+
+import com.github.mvysny.karibudsl.v10.*
+import com.github.mvysny.kaributools.setPrimary
+import com.google.gson.JsonObject
+import com.vaadin.flow.component.Key
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.Route
+import eu.vaadinonkotlin.restclient.VokRestClient
+import eu.vaadinonkotlin.restclient.VokRestClient.gson
+import eu.vaadinonkotlin.restclient.buildRequest
+import eu.vaadinonkotlin.restclient.buildUrl
+import eu.vaadinonkotlin.restclient.exec
+import eu.vaadinonkotlin.restclient.jsonMap
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("/greetings")
+class MainView : KComposite() {
+    private lateinit var nameField: TextField
+    private lateinit var greetButton: Button
+
+    // The main view UI definition
+    private val root = ui {
+        // Use custom CSS classes to apply styling. This is defined in styles.css.
+        verticalLayout(classNames = "centered-content") {
+
+            // Use TextField for standard text input
+            nameField = textField("Your name") {
+                addClassName("bordered")
+            }
+
+            // Use Button for a clickable button
+            greetButton = button("Say hello") {
+                setPrimary(); addClickShortcut(Key.ENTER)
+            }
+        }
+    }
+
+    init {
+        // attach functionality to the UI components.
+        // It's a good practice to keep UI functionality separated from UI definition.
+
+        // Button click listeners can be defined as lambda expressions
+        greetButton.onClick {
+            val vokRestClient = VokRestClient.httpClient
+            val request = "http://localhost:7070/plt/configserver/health".buildUrl().buildRequest()
+            val response = vokRestClient.exec(request){ t -> t.body().buffered().reader().readText()}
+            val fromJson = gson.fromJson(response, Map::class.java)
+            Notification.show("$response && $fromJson" )
+        }
+    }
+}
